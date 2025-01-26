723c7d8854ad274fdededa96f33d297b2c2bd7a8
Your Name <darkomilanovski45@gmail.com>
2025-01-26 23:01:49 +0100

    new

--- javascript/advance-homeworks/second-homework/script.js
+++ javascript/advance-homeworks/second-homework/script.js
@@ -1,134 +1,91 @@
-const button = document.getElementById(`button`);
-const tableDiv = document.getElementById(`table-div`);
+const button = document.getElementById("button");
+const tableDiv = document.getElementById("table-div");
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
 
-
-
-
-const generateTable = (planetsInfo) =>{
-    const headers = [`Planet Name`, `Population`, `Climate`, `Gravity`]
-    const table = document.createElement(`table`);
-    const headerRows = document.createElement(`tr`);
-    for(let i = 0; i < headers.length; i++){
-        const th = document.createElement(`th`);
-        th.innerText = headers[i];
-        headerRows.appendChild(th);
-        th.style.border = "1px solid black";
-        th.style.padding = "10px";
-        th.style.textAlign = "center";
-    }
-    table.appendChild(headerRows)
-
-    const rows = document.createElement(`tr`);
-    const information = [
-        planetsInfo.name,
-        planetsInfo.population,
-        planetsInfo.climate,
-        planetsInfo.gravity
-    ]
-    for(let j = 0; j < information.length; j++){
-        let td = document.createElement(`td`);
-        td.innerText = information[j];
-        rows.appendChild(td);
-        td.style.border = "1px solid black";
-        td.style.padding = "10px";
-        td.style.textAlign = "center";
-    }
-    table.appendChild(rows);
+const generateTable = (planetsData) => {
+  let table = document.querySelector("#planetTable");
+  if (!table) {
+    table = document.createElement("table");
+    table.id = "planetTable";
     tableDiv.appendChild(table);
-    table.style.border = `5px solid black`
-    table.style.width = `50%`
+    table.style.border = `5px solid black`;
 
-    
-}
-
-const nextTable = (nextPlanetsInfo) =>{
-    const headers = [`Planet Name`, `Population`, `Climate`, `Gravity`]
-    const table = document.createElement(`table`);
-    const headerRows = document.createElement(`tr`);
-    for(let i = 0; i < headers.length; i++){
-        const th = document.createElement(`th`);
-        th.innerText = headers[i];
-        headerRows.appendChild(th);
-        th.style.border = "1px solid black";
-        th.style.padding = "10px";
-        th.style.textAlign = "center";
+    const headers = [`Planet Name`, `Population`, `Climate`, `Gravity`];
+    const headerRow = document.createElement("tr");
+    for (let i = 0; i < headers.length; i++) {
+      const th = document.createElement("th");
+      th.innerText = headers[i];
+      headerRow.appendChild(th);
+      th.style.border = "1px solid black";
+      th.style.padding = "10px";
+      th.style.textAlign = "center";
     }
-    table.appendChild(headerRows)
+    table.appendChild(headerRow);
+  } else {
+    table.innerHTML = "";
+    const headers = [`Planet Name`, `Population`, `Climate`, `Gravity`];
+    const headerRow = document.createElement("tr");
+    for (let i = 0; i < headers.length; i++) {
+      const th = document.createElement("th");
+      th.innerText = headers[i];
+      headerRow.appendChild(th);
+      th.style.border = "1px solid black";
+      th.style.padding = "10px";
+      th.style.textAlign = "center";
+    }
+    table.appendChild(headerRow);
+  }
 
-    const rows = document.createElement(`tr`);
+  planetsData.results.forEach((planet) => {
+    const row = document.createElement("tr");
     const information = [
-        nextPlanetsInfo.name,
-        nextPlanetsInfo.population,
-        nextPlanetsInfo.climate,
-        nextPlanetsInfo.gravity
-    ]
-    for(let j = 0; j < information.length; j++){
-        let td = document.createElement(`td`);
-        td.innerText = information[j];
-        rows.appendChild(td);
-        td.style.border = "1px solid black";
-        td.style.padding = "10px";
-        td.style.textAlign = "center";
-    }
-    table.appendChild(rows);
-    tableDiv.appendChild(table);
-    table.style.border = `5px solid black`
-    table.style.width = `50%`
-}
+      planet.name,
+      planet.population,
+      planet.climate,
+      planet.gravity,
+    ];
 
-const fetchingNextApi = (newApi) => {
-    fetch(newApi)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(nextPlanetsData){
-        nextPlanetsData.results.forEach(planet => {
-            nextTable(planet);
-        })
-    })
-    .catch(function(error){
-        console.error(`Error fetching data:`, error);
-    })
-}
+    for (let j = 0; j < information.length; j++) {
+      let td = document.createElement("td");
+      td.innerText = information[j];
+      row.appendChild(td);
+      td.style.border = "1px solid black";
+      td.style.padding = "10px";
+      td.style.textAlign = "center";
+    }
+    table.appendChild(row);
+  });
+};
 
 const fetchingAppi = (api) => {
-    fetch(api)
-    .then(function(response) {
-        return response.json();  
+  fetch(api)
+    .then((response) => response.json())
+    .then((planetsData) => {
+      generateTable(planetsData);
     })
-    .then(function(planetsData) {
-        
-        planetsData.results.forEach(planet => {
-            generateTable(planet);  
-        });
-    })
-    .catch(function(error) {
-        console.error('Error fetching data:', error);  
-    });
-}
+};
 
-button.addEventListener(`click`, function(){
-    const api = `https://swapi.py4e.com/api/planets/?page=1`;
-    fetchingAppi(api);
-    const newButton = document.createElement(`button`);
-    newButton.textContent = `next`;
-    tableDiv.appendChild(newButton);
-    button.remove();
+button.addEventListener("click", function () {
+  const api = `https://swapi.py4e.com/api/planets/?page=1`;
+  fetchingAppi(api);
+  nextBtn.hidden = false;
+  prevBtn.hidden = true;
+  button.remove();
+});
 
-    newButton.addEventListener(`click`, function(){
-        const newApi = `https://swapi.py4e.com/api/planets/?page=2`;
-        tableDiv.innerHTML = ``;
-        fetchingNextApi(newApi);
-        const previousButton = document.createElement(`button`);
-        previousButton.textContent = `previous`;
-        tableDiv.appendChild(previousButton);
+nextBtn.addEventListener("click", function () {
+  const api = `https://swapi.py4e.com/api/planets/?page=2`;
+  fetchingAppi(api);
+  nextBtn.hidden = true;
+  prevBtn.hidden = false;
+  button.remove();
+});
 
-        previousButton.addEventListener(`click`, function(){
-            tableDiv.innerHTML = ``
-            fetchingAppi(api);
-            
-        })
-    })
-        
-    })
-
+prevBtn.addEventListener("click", function () {
+  const api = `https://swapi.py4e.com/api/planets/?page=1`;
+  fetchingAppi(api);
+  nextBtn.hidden = false;
+  prevBtn.hidden = true;
+  button.remove();
+});
